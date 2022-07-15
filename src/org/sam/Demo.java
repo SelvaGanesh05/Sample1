53a8593bfb45cd9a5dc2c339749e0cf89556e07f
SelvaGanesh05 <selvaganesh128@gmail.com>
2022-07-15 22:24:35 +0530

    Done Sample

---
+++ src/org/sam/Demo.java
@@ -0,0 +1,40 @@
+package org.sam;
+
+public class Demo {
+
+	
+	private void tc1() {
+System.out.println("Test case 1");
+	}
+	
+	
+	private void tc2() {
+System.out.println("Test case 2");
+	}
+	
+	private void tc3() {
+System.out.println("Test case 3");
+	}
+	
+	private void tc4() {
+System.out.println("Test case 4");
+	}
+	
+	private void tc5() {
+System.out.println("Test case 5");
+	}
+	
+	
+	public static void main(String[] args) {
+		Demo d= new Demo();
+		d.tc1();
+		d.tc2();
+		d.tc3();
+		d.tc4();
+		d.tc5();
+		
+	}
+	
+	
+	
+}
